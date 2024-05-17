88f943bfe90d62f7d306186719d90698235db148
tranx897 <63356332+tranx897@users.noreply.github.com>
2024-05-17 21:49:41 +0000

    completed users index, created users show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
   def index
-    render({ :template => "user_templates/index.html.erb"})
+    matching_users = User.all
+    @list_of_users = matching_users.order({ :username => :asc})
+    render({ :template => "user_templates/index"})
   end
+
+  def show
+    @username = params['path_username']
+    @user = User.where({ :username => @username})[0]
+    render({ :template => "user_templates/show" })
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
   get("/users", { :controller => "users", :action => "index"})
+  get("/users/:path_username", { :controller => "users", :action => "show"})
 
 end
