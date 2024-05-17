d3b3e0319313e832920108d4158f101cef50fb03
tranx897 <63356332+tranx897@users.noreply.github.com>
2024-05-17 23:57:49 +0000

    Changed method to "post" for create and update actions

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,9 @@ Rails.application.routes.draw do
 
   get("/photos", { :controller => "photos", :action => "index"})
   get("/photos/:path_photo_id", { :controller => "photos", :action => "show"})
-  get("/add_photo", { :controller => "photos", :action => "add"})
   get("/delete_photo/:path_photo_id", { :controller => "photos", :action => "delete"})
+  post("/add_photo", { :controller => "photos", :action => "add"})
   post("/update_photo/:path_photo_id", { :controller => "photos", :action => "update"})
 
-  get("/add_comment", { :controller => "comments", :action => "add"})
+  post("/add_comment", { :controller => "comments", :action => "add"})
 end
